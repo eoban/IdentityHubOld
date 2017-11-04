4c2213a2023ee04499a6c592cef81c9c54c823d6
Greg Silber <gsilber@cyberdaptive.com>
2017-11-04 17:08:40 -0400

    stuff

--- IdentityHub.UI/src/app/security/login/login.component.ts
+++ IdentityHub.UI/src/app/security/login/login.component.ts
@@ -1,4 +1,6 @@
+import { SecurityApiUri } from './../security.constants';
 
+import { Router, Params } from '@angular/router';
 import { SecurityService } from './../security.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
@@ -13,19 +15,23 @@ export class LoginComponent implements OnInit {
   @Input() callbackUrl;
   private baseUrl: string;
   private landingUrl: SafeResourceUrl;
-  constructor(private _secSvc: SecurityService, private _sanitizer: DomSanitizer) {
-    this.callbackUrl = window.location.href;
-   }
+  private token= 'a';
 
+  constructor(private _secSvc: SecurityService, private _sanitizer: DomSanitizer, private _router: Router) {
+    this.callbackUrl = window.location.href;
+  }
   ngOnInit() {
     this.baseUrl = this._secSvc.getAPiUrl();
     this.landingUrl = this._sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + '/ui/login?callbackUrl=' + this.callbackUrl);
   }
-  onNavigate(e){
-    if (e.baseURI){
-      const token = new URL(e.contentWindow.location.href).searchParams.get('token');
-      console.log(token);
-      // here we want to retrieve the user using the token store it in local storage using the service, then navigate to the home page
+
+  onNavigate(e: any) {
+    const url2 = 'http://localhost' + this._router.url;
+    const url = e.contentWindow.location.href;
+    this.token = new URL(url2).searchParams.get('token');
+    if (!this.token) {
+      this.token = new URL(url).searchParams.get('token');
     }
+    // here we want to retrieve the user using the token store it in local storage using the service, then navigate to the home page
   }
 }
